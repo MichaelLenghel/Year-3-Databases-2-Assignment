c4966d2fb3edc73078b07fa527622fcd2e478b32
povkub@gmail.com <povkub@gmail.com>
2018-11-10 21:24:37 +0000

    my PL/SQL done

--- plSQL.sql
+++ plSQL.sql
@@ -474,6 +474,7 @@ INSERT INTO BuyTransaction (buyTransID, companyID, agentID, propertyID, buyerID,
 INSERT INTO BuyTransaction (buyTransID, companyID, agentID, propertyID, buyerID, sellerID)
     VALUES(3, 1, 7, 13, 5, 10);
 
+-- SELLER PL/SQL
 SET SERVEROUTPUT ON 
 DECLARE
     --Increment propertyid and sellerid, rather than input
@@ -514,6 +515,30 @@ DECLARE
     End;
 
 
+--BUYER PL/SQL
+SET SERVEROUTPUT ON
+DECLARE
+    maxPrice Property.price%TYPE :='&Max_Price';
+    bedrooms Property.numBedrooms%TYPE :='&Number_of_bedrooms';
+BEGIN
+    FOR prop IN (SELECT address, price, numBedrooms, numToilets, hasBalcony, hasGarden
+    FROM Property
+    WHERE numBedrooms = bedrooms AND price <= maxPrice)
+    LOOP
+    dbms_output.put_line('Address: ' || prop.address); 
+    dbms_output.put_line('Price: $' || prop.price);
+    dbms_output.put_line('Number of Bedrooms: ' || prop.numBedrooms);
+    dbms_output.put_line('Number of toilets' || prop.numToilets);
+    dbms_output.put_line('Balcony: ' || prop.hasBalcony);
+    dbms_output.put_line('Garden: ' || prop.hasGarden);
+    dbms_output.put_line('');
+    END LOOP;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+    dbms_output.put_line('Nothing Found');
+    WHEN OTHERS THEN
+    dbms_output.put_line('Error');
+END; 
 
 
 
