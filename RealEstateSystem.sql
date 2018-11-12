b4c3c10328c9ce83c4b6f76f6a77e4104c30b0bc
mondo192 <31599023+mondo192@users.noreply.github.com>
2018-11-12 21:31:50 +0000

    SQL Modeler Diagrams

--- RealEstateSystem.sql
+++ RealEstateSystem.sql
@@ -20,6 +20,7 @@ CREATE TABLE Company (
 );
 
 -- eamonn pl-sql for this 
+-- updates for sale for rent table and puts in into transaction
 CREATE TABLE EstateAgent (
     agentID NUMBER(5) NOT NULL,
     agentName VARCHAR2(50) NOT NULL,
@@ -217,10 +218,10 @@ INSERT INTO Buyer (buyerID, buyerName, buyerPhoneNum, buyerEmail, minPreferredPr
     VALUES (9, 'Michael Lenghel', '4946345791', 'michael@buyer.ie', 500000, 950000, 3, 4, 10, 1);
     
 INSERT INTO ForSale (saleID, askingPrice, propertyID) VALUES (1, 100000, 6);
-INSERT INTO ForSale(saleID, askingPrice, propertyID) VALUES(2, 550000, 3);
+INSERT INTO ForSale (saleID, askingPrice, propertyID) VALUES(2, 550000, 3);
     
-INSERT INTO ForRent(rentID, monthlyRent, propertyID) VALUES(1, 660, 1);
-INSERT INTO ForRent(rentID, monthlyRent, propertyID) VALUES(2, 1100, 5);
+INSERT INTO ForRent (rentID, monthlyRent, propertyID) VALUES(1, 660, 1);
+INSERT INTO ForRent (rentID, monthlyRent, propertyID) VALUES(2, 1100, 5);
 
 INSERT INTO RentTransaction (rentTransID, companyID, propertyID, agentID, buyerID, sellerID) VALUES (1, 1, 14, 10, 7, 8);
 INSERT INTO RentTransaction (rentTransID, companyID, propertyID, agentID, buyerID, sellerID) VALUES (2, 1, 15, 5, 8, 1);
@@ -242,3 +243,39 @@ select * from forsale;
 select * from property;
 select * from renttransaction;
 select * from seller;
+
+select rentid, monthlyrent, propertyid, count(*) over() count_all
+from forrent;
+
+select * from renttransaction;
+select * from forrent;
+
+
+-- EstateAgent PL/SQL
+SET SERVEROUTPUT ON
+DECLARE
+    V_NO EstateAgent.agentID%TYPE:='&Agent_Number';
+    V_NAME EstateAgent.agentName%TYPE;
+    V_RENTABLE INTEGER;
+    V_FORRENT ForRent.propertyID%TYPE:='&Rentable_Property';
+BEGIN
+    SELECT agentName INTO V_NAME FROM EstateAgent WHERE agentID = V_NO;
+    SELECT ForRent.propertyID, COUNT(*) OVER() Count_All INTO V_FORRENT, V_RENTABLE FROM ForRent WHERE propertyid = V_FORRENT;
+
+    IF (V_RENTABLE > 0) THEN
+        DBMS_OUTPUT.PUT_LINE(V_RENTABLE || ' available properties for rent');
+        INSERT INTO RentTransaction (rentTransID, companyID, propertyID, agentID, buyerID, sellerID)
+            VALUES (4, 1, V_FORRENT, V_NO, 1, 5);
+        DELETE FROM ForRent WHERE ForRent.propertyID = V_FORRENT;
+        COMMIT;
+    ELSE 
+        DBMS_OUTPUT.PUT_LINE(V_RENTABLE || ' properties on market for rent. Transaction cancelled');
+    END IF;
+    
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('No such data found');
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Rolling back');
+    ROLLBACK;
+END;
